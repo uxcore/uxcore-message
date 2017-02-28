74c6ce4add3377b652bea99f9a37772d7409a2ff
wenzhao.fw <wenzhao.fw@alibaba-inc.com>
2017-02-28 20:51:45 +0800

    fix issue #14 & 15

--- demo/MessageDemo.js
+++ demo/MessageDemo.js
@@ -6,34 +6,38 @@
  * All rights reserved.
  */
 
-let classnames = require('classnames');
 
-let Message = require('../src');
-let Button = require('uxcore-button');
+const Message = require('../src');
+const Button = require('uxcore-button');
 
 class Demo extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    }
+  constructor(props) {
+    super(props);
+    /*
+    // test issue #14
+    Message.config({
+      multiple: false,
+    });
+    */
+  }
 
-    handleClick(type) {
-        Message[type](type, 10)
-    }
+  handleClick(type) {
+    Message[type](type, 10);
+  }
 
-    render() {
-        let me = this;
-        return (
-            <div>
-                <Button onClick={me.handleClick.bind(me, 'success')}>显示成功提示</Button>
-                <Button onClick={me.handleClick.bind(me, 'info')}>显示普通提示</Button>
-                <Button onClick={me.handleClick.bind(me, 'error')}>显示错误提示</Button>
-                <Button onClick={me.handleClick.bind(me, 'loading')}>显示加载提示</Button>
-            </div>
-        );
-    }
-};
+  render() {
+    const me = this;
+    return (
+      <div>
+        <Button onClick={me.handleClick.bind(me, 'success')}>显示成功提示</Button>
+        <Button onClick={me.handleClick.bind(me, 'info')}>显示普通提示</Button>
+        <Button onClick={me.handleClick.bind(me, 'error')}>显示错误提示</Button>
+        <Button onClick={me.handleClick.bind(me, 'loading')}>显示加载提示</Button>
+        <Button onClick={me.handleClick.bind(me, 'clear')}>清除</Button>
+      </div>
+    );
+  }
+}
 
 module.exports = Demo;
